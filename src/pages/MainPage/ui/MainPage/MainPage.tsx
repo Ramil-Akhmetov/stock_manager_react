1503bbfb711af73e9baa96b4cf6a6b318ececde4
Ramil Akhmetov <ramil147258369@gmail.com>
2024-06-14 09:03:00 +0700

    done?

--- src/pages/MainPage/ui/MainPage/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage/MainPage.tsx
@@ -1,96 +1,14 @@
-import { memo } from 'react';
-import UserEntityCreateEdit from '@/pages/EntityTables/User/UserEntityCreateEdit';
+import { memo, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Page } from '@/widgets/Page';
-import { AboutProgram } from '@/features/AboutProgram';
-import { CategoryEdit } from '@/features/CategoryEdit';
-import {
-  useCreateCategory,
-  useEditCategory,
-  useGetCategory,
-} from '@/features/CategoryEdit/api/categoryEditApi.ts';
-import { EntityTable } from '@/features/EntityTable';
-import { DataForm } from '@/shared/ui/DataForm';
 
 function MainPage() {
-  return (
-    <Page>
-      {/* <AboutProgram /> */}
-      {/* <ItemsTable /> */}
-      {/* <UsersTable /> */}
-      {/* <CategoryEdit categoryId={1} /> */}
+  const navigate = useNavigate();
 
-      {/*  <DataForm */}
-      {/*    title="" */}
-      {/*    fields={[ */}
-      {/*      { */}
-      {/*        name: 'name', */}
-      {/*        label: 'Название', */}
-      {/*        type: 'input', */}
-      {/*      }, */}
-      {/*      { */}
-      {/*        name: 'code', */}
-      {/*        label: 'code', */}
-      {/*        type: 'input', */}
-      {/*      }, */}
-      {/*    ]} */}
-      {/*    dataFetchQuery={useGetCategory} */}
-      {/*    dataEditQuery={useEditCategory} */}
-      {/*    dataSubmitQuery={() => { */}
-      {/*      return [ */}
-      {/*        () => { */}
-      {/*          return {}; */}
-      {/*        }, */}
-      {/*        { */}
-      {/*          data: {}, */}
-      {/*        }, */}
-      {/*      ]; */}
-      {/*    }} */}
-      {/*    id={1} */}
-      {/*  /> */}
-
-      {/*  <DataForm */}
-      {/*    title="" */}
-      {/*    fields={[ */}
-      {/*      { */}
-      {/*        name: 'name', */}
-      {/*        label: 'Название', */}
-      {/*        type: 'input', */}
-      {/*      }, */}
-      {/*      { */}
-      {/*        name: 'code', */}
-      {/*        label: 'code', */}
-      {/*        type: 'input', */}
-      {/*      }, */}
-      {/*    ]} */}
-      {/*    dataFetchQuery={() => { */}
-      {/*      return { data: {} }; */}
-      {/*    }} */}
-      {/*    dataEditQuery={() => { */}
-      {/*      return [ */}
-      {/*        () => { */}
-      {/*          return {}; */}
-      {/*        }, */}
-      {/*        { */}
-      {/*          data: {}, */}
-      {/*        }, */}
-      {/*      ]; */}
-      {/*    }} */}
-      {/*    dataSubmitQuery={() => { */}
-      {/*      return [ */}
-      {/*        () => { */}
-      {/*          return {}; */}
-      {/*        }, */}
-      {/*        { */}
-      {/*          data: {}, */}
-      {/*        }, */}
-      {/*      ]; */}
-      {/*    }} */}
-      {/*  /> */}
-
-      {/* <EntityTable /> */}
-      {/* <CheckoutEntityCreateEdit /> */}
-    </Page>
-  );
+  useEffect(() => {
+    navigate('/items');
+  }, [navigate]);
+  return <Page />;
 }
 
 export default memo(MainPage);
